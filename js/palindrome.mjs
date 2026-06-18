59effb5dac620a4b0cab9ef1785172396090267e
Siclemon <247134392+Siclemon@users.noreply.github.com>
2026-06-18 17:00:09 +0200

    pendu + palindrome

---
+++ js/palindrome.mjs
@@ -0,0 +1,19 @@
+import { createInterface } from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+async function main() {
+    const rl = createInterface({ input, output });
+
+    const phrase = (await rl.question('Phrase ?\n > ')).trim().toLowerCase().replaceAll(' ','');
+
+    const palindrome = phrase.split('').every((l, i, arr) => l === arr[arr.length - i - 1])
+
+    if (palindrome)
+        console.log(`\x1b[38;2;0;255;0mc'est un palindrome!! youpi!!!\x1b[m`)
+    else
+        console.log(`\x1b[38;2;255;0;0mc'est pas un palindrome :(\x1b[m`)
+
+    rl.close();
+}
+
+await main();
---
+++ js/pendu.mjs
@@ -0,0 +1,77 @@
+import { createInterface } from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+async function main() {
+    const rl = createInterface({ input, output });
+
+    const mot = await saisieMot(rl);
+    console.clear();
+
+    let reste = mot.substring(1, mot.length - 1);
+    let lettresTrouvees = 2;
+    const trouvee = [true];
+    const lettresTestees = [];
+    let vies = 6;
+
+    for (let i = 0; i < mot.length - 2; i++)
+        trouvee.push(false);
+    trouvee.push(true);
+
+    // console.table(trouvee)
+
+    do {
+        let motAffiche = '';
+        for (let i = 0; i < mot.length; i++) {
+            if (trouvee[i])
+                motAffiche += mot[i];
+            else
+                motAffiche += '-';
+        }
+        console.log(motAffiche);
+
+        for (let i = 0; i < vies; i++)
+            rl.write(" ❤️ ");
+        rl.write("\n\n");
+
+        const lettre = await rl.question('Lettre ?\n > ');
+
+        if (lettresTestees.includes(lettre)) {
+            console.log('Lettre déjà testé nullos ╰（‵□′）╯')
+        } else {
+            lettresTestees.push(lettre)
+            if (mot.includes(lettre)) {
+                for (let i = 1; i < mot.length - 1; i++) {
+                    if (lettre === mot[i]) {
+                        trouvee[i] = true;
+                        lettresTrouvees++;
+                    }
+                }
+            } else 
+                vies--;
+        }
+        console.log()
+
+    } while (lettresTrouvees < mot.length && vies > 0);
+
+    if (vies > 0) 
+        console.log(`Gagné !! Le mot était bien ${mot}`)
+    else 
+        console.log(`perdu 🫵 😂\nle mot était ${mot} `)
+
+
+
+    rl.close();
+}
+
+async function saisieMot(rl) {
+
+    while (true) {
+        const mot = await rl.question('Quel est le mot à deviner ?\n > ');
+        if (mot.length >= 5)
+            return mot;
+        console.log('Au moins 5 lettres stp (✿ ◠ ‿◠)')
+    }
+
+}
+
+await main();
